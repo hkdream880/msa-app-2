489548a6f23cb8ce1704bd36b10840bb842c79a0
이현국 <hklee@sptek.co.kr>
2021-04-20 16:35:46 +0900

    test app 2 init

---
+++ src/test/java/com/example/secondservicce/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondservicce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
